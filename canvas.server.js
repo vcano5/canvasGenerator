e61adf1a5b8bb8a7cd3e7c6440f321181ada9066
Victor Cano <victoreduardocano5@gmail.com>
2021-03-29 05:18:53 -0600

    first commit

---
+++ canvas.server.js
@@ -0,0 +1,100 @@
+const 	fs = require('fs'),
+		https = require('https'),
+		request = require('request'),
+		Canvas = require('canvas'),
+		{Image} = require('canvas'),
+		express = require('express'),
+		app = express();
+
+
+app.listen((process.env.PORT || 3400));
+
+app.get('/', (req, res) => {
+	if(req.query.criptomoneda !== undefined) {
+		https.get(`https://api.bitso.com/v3/ticker?book=${req.query.criptomoneda}_mxn`, (resp) => {
+          let data = '';
+          resp.on('data', (chunk) => {
+            data += chunk;
+          })
+
+          resp.on('end', () => {
+            var datos = JSON.parse(data);
+			
+			console.log(datos);
+
+            const img = new Image();
+            //img.src = './failure.png'
+            img.src = './space.jpg'
+            var c24h = parseInt(datos.payload['change_24']);
+            if(c24h > 5000) {
+              img.src = './space.jpg'
+            }
+            else if(c24h > 10000) {
+              img.src = './marte.png'
+            }
+
+            var c = Canvas.createCanvas(800, 600);
+            var ctx = c.getContext('2d');
+            ctx.drawImage(img, 0, 0);
+            ctx.font = "80px Arial";
+            ctx.textAlign = "center";
+            ctx.fillStyle = "white";
+
+            ctx.fillText(`${req.query.criptomoneda.toUpperCase()}`, 400, 100);
+            ctx.font = "72px Arial";
+            ctx.lineWidth = 3;
+
+            ctx.strokeText(`${parseInt(datos.payload.last)} MXN - 1 ${req.query.criptomoneda.toUpperCase()}`, 400, 300);
+            ctx.fillText(`${parseInt(datos.payload.last)} MXN - 1 ${req.query.criptomoneda.toUpperCase()}`, 400, 300);
+           
+            
+            ctx.font = "32px Arial";
+            ctx.strokeText(`Cambio 24h: ${parseInt(datos.payload['change_24'])}`, 400, 350 )
+            ctx.fillText(`Cambio 24h: ${parseInt(datos.payload['change_24'])}`, 400, 350 )
+            
+            ctx.font = "16px Arial"
+
+
+
+            ctx.textAlign = "start";
+            ctx.fillText('Fuente: api.bitso.com', 10, 566)
+            ctx.fillText(`Ultima actualizacion: ${new Date().getHours()}:${new Date().getUTCMinutes()}:${new Date().getUTCSeconds()}`	, 10, 584);
+
+
+            var logo = new Image();
+            logo.src = './elTripLogo.png'
+            ctx.globalAlpha = 0.1
+
+            ctx.drawImage(logo, (800-128), (600-128))
+
+			res.type('png')
+			res.send(Buffer.from(c.toBuffer()))
+
+			//res.sendFile(c.toBuffer());
+
+
+            // const attachment = new MessageAttachment(c.toBuffer(), 'btc-mxn.png');
+            // if(!msg.match(varCriptos)) {
+            //   message.channel.send( attachment).then((m) => {
+            //     if(Number.parseFloat(datos.payload['change_24']).toFixed(2) > 1000) {
+            //       m.react('🚀')
+            //     }
+            //   });
+            // }
+            // else {
+            //   message.author.send(attachment).then((m) => {
+            //     if(Number.parseFloat(datos.payload['change_24']).toFixed(2) > 1000) {
+            //       m.react('🚀')
+            //     }
+            //   })
+            //   message.react('🚀')
+            // }
+          })
+        }).on("error", (err) => {
+          console.log("Error: " + err.message)
+        })
+	}
+})
+
+
+
